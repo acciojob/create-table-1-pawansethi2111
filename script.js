df7e185de3bdc2da98ee6a475ee521a4973b5b83
PAWAN SETHI <66323571+pawansethi2111@users.noreply.github.com>
2024-12-28 16:20:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
+  let table = document.getElementById('sampleTable');
+
+	var row= table.insertRow(0);
+	var cell1 = row.insertCell(0);
+	cell1.innerHTML = 'New Cell2'
+	var cell2 = row.insertCell(0);
+	cell2.innerHTML ='New Cell1';
+  let rowinsert = document.createElement('tr');
+  rowinsert.innerHTML=`<td>New Cell1</td><td>New Cell2</td>`;
   
 }
